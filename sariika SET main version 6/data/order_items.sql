d6bc57b71926b33b99a84ce9874e2d166b8a9403
Alisha Abbas <B00159822@mytudublin.ie>
2025-04-23 23:42:09 +0100

    product

--- sariika SET main version 6/data/order_items.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE order_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (`product_id`) REFERENCES products(id)
-);
-
